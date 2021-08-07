edcb4da5a937e5a700c69adda853ea1d2b4c5f98
darkness0710 <nguyenthanhhaid13cn7@gmail.com>
2021-08-07 15:48:51 +0700

    update

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -13,7 +13,11 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('client.home.index');
+        $url = "https://wescan.vn/api/v1/donator_ranks?user_id=d91652d4fa8545369da73c1c3516864e&sort_by=base_price&sort_type=desc&limit=10&from_date=2020%2F08%2F01&to_date=2023%2F08%2F07";
+        $reponse = file_get_contents($url);
+        $wescanData = \Arr::get(json_decode($reponse, true), 'data.items')[0]['ranks'] ?? [];
+
+        return view('client.home.index', compact('wescanData'));
     }
 
     public function introduction()
--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -3,57 +3,87 @@
 @section('content')
 <div class="main main-raised mt-100">
     <div class="section">
-        <div class="projects-2" id="projects-2">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 ml-auto mr-auto text-center">
-                            <h2 class="title">OusayGaming</h2>
-                            @if (isUserLogin())
-                                <h5 class="description">Chào mừng {{ getCurrentUser()->name }} đến với OusayGaming, hãy lựa chọn các chức năng của website!</h5>
-                            @else
-                                <h5 class="description">Chào mừng bạn đến với OusayGaming, hãy lựa chọn các chức năng của website!</h5>
-                            @endif
-                            <div class="section-space"></div>
-                        </div>
+        <div class="projects-2" id="projects-2" style="margin-bottom: -100px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 ml-auto mr-auto text-center">
+                        <h2 class="title">OusayGaming</h2>
+                        @if (isUserLogin())
+                            <h5 class="description">Chào mừng {{ getCurrentUser()->name }} đến với OusayGaming, hãy lựa chọn các chức năng của website!</h5>
+                        @else
+                            <h5 class="description">Chào mừng bạn đến với OusayGaming, hãy lựa chọn các chức năng của website!</h5>
+                        @endif
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="card card-plain">
-                                <a href="{{ route('client.shops.index') }}">
-                                    <div class="card-header card-header-image">
-                                        <img src="{{ asset('images/client_home_shop.jpg') }}">
-                                    </div>
-                                </a>
-                                <div class="card-body ">
-                                    <a href="{{ route('client.shops.index') }}">
-                                        <h4 class="card-title">Shop Account 3Q</h4>
-                                    </a>
-                                    <p class="card-description">
-                                        Nơi giao bán, các loại acc game 3Q!
-                                    </p>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card card-plain">
+                            <a href="{{ route('client.shops.index') }}">
+                                <div class="card-header card-header-image">
+                                    <img src="{{ asset('images/client_home_shop.jpg') }}">
                                 </div>
+                            </a>
+                            <div class="card-body ">
+                                <a href="{{ route('client.shops.index') }}">
+                                    <h4 class="card-title">Shop Account 3Q</h4>
+                                </a>
+                                <p class="card-description">
+                                    Nơi giao bán, các loại acc game 3Q!
+                                </p>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="card card-plain">
-                                <a href="{{ route('client.games.index') }}">
-                                    <div class="card-header card-header-image">
-                                        <img src="{{ asset('images/client_home_game.jpg') }}">
-                                    </div>
-                                </a>
-                                <div class="card-body ">
-                                    <a href="{{ route('client.games.index') }}">
-                                        <h4 class="card-title">Hướng dẫn game</h4>
-                                    </a>
-                                    <p class="card-description">
-                                        Nơi thảo luận, hướng dẫn chơi các loại game 3Q!
-                                    </p>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card card-plain">
+                            <a href="{{ route('client.games.index') }}">
+                                <div class="card-header card-header-image">
+                                    <img src="{{ asset('images/client_home_game.jpg') }}">
                                 </div>
+                            </a>
+                            <div class="card-body ">
+                                <a href="{{ route('client.games.index') }}">
+                                    <h4 class="card-title">Hướng dẫn game</h4>
+                                </a>
+                                <p class="card-description">
+                                    Nơi thảo luận, hướng dẫn chơi các loại game 3Q!
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>               
+            </div>
+        </div>               
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 ml-auto mr-auto">
+                   <div class="col-md-8 ml-auto mr-auto text-center">
+                    <h3 class="title">Bảng xếp hạng donate wescan</h3>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">Hạng</th>
+                                    <th class="text-center">Game Thủ</th>
+                                    <th class="text-center">Số Lần Donate</th>
+                                    <th class="text-center">Tổng số tiền Donate</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($wescanData as $idx => $data)
+                                    <tr>
+                                        <td class="text-center">{{ ++$idx }}</td>
+                                        <td class="text-center">{{ $data['name'] }}</td>
+                                        <td class="text-center">{{ $data['times'] }}</td>
+                                        <td class="text-center">{{ number_format($data['amount']) . ' VNĐ' }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
