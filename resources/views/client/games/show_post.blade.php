504e6c4059447ef850037af4c78d5c82c25ec2a3
darkness0710 <nguyenthanhhaid13cn7@gmail.com>
2021-08-07 16:39:31 +0700

    update

--- resources/views/client/games/show_post.blade.php
+++ resources/views/client/games/show_post.blade.php
@@ -21,6 +21,7 @@
                 </div>
                 <div class="row">
                     <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fshopacc3q.com%2F&width=450&layout=standard&action=like&size=large&share=true&height=35&appId=1058991607967887" width="450" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    <div class="fb-comments" data-href="http://shopacc3q.com/" data-width="" data-numposts="100"></div>
                 </div>
                 {{-- <div class="row">
                     <div class="col-md-8 ml-auto mr-auto text-right">
